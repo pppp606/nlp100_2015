7d4791e928aabd5eb003e0292a9c2034f4cccfca
pppp606 <ppppp303@gmail.com>
2015-08-06 21:43:12 +0900

    chapter3

--- main.js
+++ main.js
@@ -251,3 +251,132 @@ for (var i = 0; i < strings.length; i++) {
 answer = answer.join(" ");
 console.log("09------------------------------------------------");
 console.log(answer);
+
+// ----------------------------------------------------------------------------------------------
+// 09. Typoglycemia
+// スペースで区切られた単語列に対して，各単語の先頭と末尾の文字は残し，それ以外の文字の順序をランダムに並び替えるプログラムを作成せよ．ただし，長さが４以下の単語は並び替えないこととする．
+// 適当な英語の文（例えば"I couldn't believe that I could actually understand what I was reading : the phenomenal power of the human mind ."）を与え，その実行結果を確認せよ．
+// ----------------------------------------------------------------------------------------------
+
+/**
+記事データを取得し整形
+* @class MyWikipediaAPI
+* @property {has} datas 記事タイトルをキーに持つ整形済みデータ
+* @property {string} url 記事データのULR
+* @param {strings} url 記事データのULR
+*/
+var MyWikipediaAPI = function(url){
+  this.datas = [];
+  this.url = url;
+  /**
+  * ajaxでdata取得
+  * @method getData
+  * @return jsonデータ
+  */
+  this.getData = $.ajax({
+      url:this.url,
+      type:'GET',
+      cache:true,
+      context:this,
+      dataType:'text'
+    });
+  /**
+  * データの整形
+  * @method shaping
+  * @param {string} t 行分割したテキスト
+  */
+  this.shaping = function(t){
+    //カテゴリ(問21,22)
+    t.categoles = t.text.match(/\[\[カテゴリ:[^\]]*?\]\]/g);
+    if(t.categoles){
+      for (var j = 0; j < t.categoles.length; j++) {
+        t.categoles[j] = t.categoles[j].replace(/\[\[カテゴリ:([^\]]*?)\]\]/,'$1');
+      }
+    }
+    //セクション(問23)
+    t.sections = t.text.match(/={2,} [^=]*? ={2,}/g);
+    if(t.sections){
+      for (var j = 0; j < t.sections.length; j++) {
+        _level = t.sections[j].replace(/(={2,}).+/,'$1').length - 1
+        t.sections[j] = {
+          level:_level,
+          name:t.sections[j].replace(/^={2,} (.*?) ={2,}$/,'$1')
+        }
+      }
+    }
+    //ファイル(問24)
+    t.files = t.text.match(/\[\[ファイル:[^\]]*?\]\]/g);
+    if(t.files){
+      for (var j = 0; j < t.files.length; j++) {
+         _datas = t.files[j].replace(/\[\[ファイル:([^\]]*?)\]\]/,'$1').split("|");
+         t.files[j] = {
+           name:_datas[0],
+           size:_datas[1],
+           alt:_datas[2]
+         }
+      }
+    }
+    //基礎情報(問25~29)
+    if(t.text.match(/{{基礎情報[\s\S]*?}}\n/g)){
+      //基礎情報スタート位置
+      var _lines = t.text.replace(/[\s\S]*{{基礎情報 国([\s\S]*)/g,'$1').replace(/\|\n/g,'\n|').split("\n|");
+      var _setLines = []
+      for (var j = 0; j < _lines.length; j++) {
+        var targetLine = _lines[j];
+        //問25
+        targetLine = targetLine.trim().replace('|','');
+        //基礎情報が終わるポイント
+        if(targetLine.indexOf("}}\n") == 0){
+          j = _lines.length + 1;
+        }else if(targetLine.indexOf("}}\n") > 0){
+          targetLine = targetLine.split("}}\n")[0];
+          j = _lines.length + 1;
+        }
+        if(j<_lines.length && targetLine != ""){
+          //問26
+          targetLine = targetLine.replace(/'{2,}/g,'');
+          //問27
+          var _reg = /\[\[(.*?)\]\]/g;
+          var _rep = "__reg__link__ptn__";
+          var _links = targetLine.match(_reg);
+          if(_links){
+            targetLine = targetLine.replace(_reg,_rep);
+            for (var k = 0; k < _links.length; k++) {
+              var _cnp = _links[k].replace(_reg,'$1').split("|");
+              targetLine = targetLine.replace(_rep,_cnp[_cnp.length - 1]);
+            }
+          }
+          //問28
+          targetLine = targetLine
+            .replace(/<("[^"]*"|'[^']*'|[^'">])*>/g,'')
+            .replace(/{{[^\|]*?\|/g,'')
+            .replace(/}}/g,'')
+            .replace(/\[[^\[]*?\]/g,'');
+
+          //キー配列に変換
+          var _name = targetLine.replace(/([^=]*?)=[\s\S]*/,'$1').trim();
+          var _val = targetLine.replace(/.*?=([\s\S]*)/,'$1').trim().replace(/\n/,'');
+          _setLines[_name] = _val;
+        }
+      }
+      t.baseData = _setLines;
+    }
+    this.datas[t.title] = t;
+  }
+  /**
+  * 国旗画像のデータを取得
+  * @method getFlagImg
+  * @param {string} name 国名
+  * @return jsonデータ
+  */
+  this.getFlagImg = function(name){
+    if(this.datas[name].baseData["国旗画像"]){
+      return $.ajax({
+          url: 'https://en.wikipedia.org/w/api.php?action=query&titles=File:' + encodeURIComponent(this.datas[name].baseData["国旗画像"]) + '&prop=imageinfo&iiprop=url&format=json',
+          type:'get',
+          context:this,
+          dataType:'jsonp'
+      });
+    }
+  }
+}
